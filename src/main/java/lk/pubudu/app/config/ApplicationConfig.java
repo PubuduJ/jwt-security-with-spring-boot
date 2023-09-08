edcd4e2f686b41b1d601a1e3fae0ace5ee5689a9
Pubudu Janith <pubudujanith123@gmail.com>
2023-09-08 17:23:35 +0530

    Complete ApplicationConfig configuration class code logic

--- src/main/java/lk/pubudu/app/config/ApplicationConfig.java
+++ src/main/java/lk/pubudu/app/config/ApplicationConfig.java
@@ -4,8 +4,14 @@ import lk.pubudu.app.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -17,6 +23,26 @@ public class ApplicationConfig {
     @Bean
     public UserDetailsService userDetailsService() {
         return username -> userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+    // AuthenticationProvider is DataAccessObject which is used to fetch the user details, encode password etc.
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
+    }
+
+    // AuthenticationManager is responsible to manage the Authentication.
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
---
+++ src/main/java/lk/pubudu/app/config/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package lk.pubudu.app.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfiguration {
+    // This class is used to bind the JwtAuthenticationFilter with the Spring Boot Backend.
+    // At the application startup, Spring will try to look up a bean of SecurityFilterChain.
+    // SecurityFilterChain bean is responsible for configuring all the http security of the application.
+
+    private final JwtAuthenticationFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf()
+                .disable()
+                .authorizeHttpRequests()
+                .requestMatchers("/api/v1/auth/**")                // All the requests which comes to these end points will not be authenticated. (No need of token)
+                .permitAll()
+                .anyRequest()                                               // All the other requests will be authenticated.
+                .authenticated()
+                .and()                                                      // and will add new configuration.
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)     // session will be stateless.
+                .and()
+                .authenticationProvider(authenticationProvider)             //
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class); // add created JwtAuthenticationFilter before UsernamePasswordAuthenticationFilter.
+                // Because after updating the security context holder at the end of the JwtAuthenticationFilter, it will call the UsernamePasswordAuthenticationFilter next.
+        return httpSecurity.build();
+    }
+}
