9e77ecf3f882ace51747648ad3f65f05172fe26f
Pubudu Janith <pubudujanith123@gmail.com>
2023-09-08 16:38:30 +0530

    Create ApplicationConfig class

---
+++ src/main/java/lk/pubudu/app/config/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package lk.pubudu.app.config;
+
+import lk.pubudu.app.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+    // This class is used to hold application configurations.
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+}
--- src/main/java/lk/pubudu/app/config/JwtAuthenticationFilter.java
+++ src/main/java/lk/pubudu/app/config/JwtAuthenticationFilter.java
@@ -6,6 +6,11 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -18,6 +23,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     // Every time gets a request to the backend, Jwt authentication filter will be executed.
 
     private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
     @Override
     protected void doFilterInternal(
@@ -38,6 +44,24 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         final String jwtToken = authHeader.substring(7);
         // Extract userEmail from the JWT token.
         final String userEmail = jwtService.extractUsername(jwtToken);
+
+        // Check whether userEmail is present and user is not authenticated yet by using SecurityContextHolder.getContext().getAuthentication() == null.
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            // Get user from the database as user details.
+            UserDetails userDetails = this.userDetailsService.loadUserByUsername(userEmail);
+
+            // If token is valid we need to update the security context holder and send the request to dispatcher servlet.
+            if (jwtService.isTokenValid(jwtToken, userDetails)) {
+
+                // Create object of type UsernamePasswordAuthenticationToken as userDetails, credentials and authorities as parameters.
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                // set details to authToken from our request.
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                // Update the security context holder.
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
 
 }
