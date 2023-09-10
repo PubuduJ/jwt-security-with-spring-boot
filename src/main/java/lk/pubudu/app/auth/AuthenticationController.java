dcd6aa31f8b62feb2bed858e11cc9a36cd851e0e
Pubudu Janith <pubudujanith123@gmail.com>
2023-09-10 19:05:17 +0530

    Create AuthenticationController class

---
+++ src/main/java/lk/pubudu/app/auth/AuthenticationController.java
@@ -0,0 +1,23 @@
+package lk.pubudu.app.auth;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request) {
+        return null;
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody AuthenticationRequest request) {
+        return null;
+    }
+}
