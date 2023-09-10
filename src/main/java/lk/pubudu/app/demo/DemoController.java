8b5bc7edd8ccc14f1f6314e792b52ead20eacd5d
Pubudu Janith <pubudujanith123@gmail.com>
2023-09-10 19:32:21 +0530

    Create DemoController class

---
+++ src/main/java/lk/pubudu/app/demo/DemoController.java
@@ -0,0 +1,17 @@
+package lk.pubudu.app.demo;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/demo-controller")
+public class DemoController {
+
+    @GetMapping
+    public ResponseEntity<String> sayHello() {
+        return ResponseEntity.status(HttpStatus.OK).body("Hello from secured endpoint");
+    }
+}
